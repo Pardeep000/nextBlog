a21d6e8ba3b93419a12e4d5296358d8067689a42
Pardeep000 <pardeepkumar1727@gmail.com>
2023-05-31 17:59:19 +0500

    little more done

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
+"use client"
 import styles from './page.module.css'
-import fs from 'fs'
-import path from 'path'
+import React, { useEffect, useState } from 'react'
+import { Grid, Box, ListItem } from '@mui/material';
+import CardComp from "./components/card/Card"
+import { CircularProgress } from '@mui/material';
+
+
+
+interface dataProp {
+  title: String
+  desc: String
+  author: String
+  date: String
+  img: String
+  position: String
+}
 
 export default function Home() {
-  
-  const dir = path.join(process.cwd(),'app/blogposts')
-  const fileNames = fs.readdirSync(dir);
-  console.log("fileNames: ",fileNames)
+
+  const [blogdata, setBlogdata] = useState<null | dataProp[]>(null)
+
+  useEffect(() => {
+    fetch("http://www.localhost:3000/api/data").then(result => result.json()).then(data => setBlogdata(data.content))
+  }, [])
 
   return (
     <main className={styles.main}>
-      home page is here
+      <Box>
+        <Grid container spacing={2}>
+          {blogdata === null ? <CircularProgress color="secondary" size={30}/> : blogdata.map(item => <Grid item xs={item.position === "top" ? 6 : 4} style={{ paddingLeft: item.position === "top" ? "8%" : "2%" }}>
+            <CardComp title={item.title} desc={item.desc} date={item.date} author={item.author} img={item.img} />
+          </Grid>)}
+        </Grid>
+      </Box>
     </main>
   )
 }
